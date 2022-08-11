abe1e84aee2f847cabd60ec3e17eb18f9affb476
KEVIN BEAUCORAL <beaucoral.kevin@gmail.com>
2022-08-11 11:41:45 +0200

    fix(mixin): use redirect to change the background texture of canvas sign

--- src/main/java/com/nhoryzon/mc/farmersdelight/mixin/CanvasSignEditScreenMixin.java
+++ src/main/java/com/nhoryzon/mc/farmersdelight/mixin/CanvasSignEditScreenMixin.java
@@ -4,12 +4,14 @@ import com.nhoryzon.mc.farmersdelight.block.signs.ICanvasSign;
 import com.nhoryzon.mc.farmersdelight.client.render.block.CanvasSignBlockEntityRenderer;
 import net.minecraft.block.entity.SignBlockEntity;
 import net.minecraft.client.gui.screen.ingame.SignEditScreen;
+import net.minecraft.client.render.TexturedRenderLayers;
 import net.minecraft.client.util.SpriteIdentifier;
+import net.minecraft.util.SignType;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(SignEditScreen.class)
 public class CanvasSignEditScreenMixin {
@@ -18,13 +20,14 @@ public class CanvasSignEditScreenMixin {
     @Final
     private SignBlockEntity sign;
 
-    @ModifyVariable(at = @At(value = "STORE"), method = "render", name = "spriteIdentifier")
-    public SpriteIdentifier useCanvasSignSprite(SpriteIdentifier signSprite) {
+    @Redirect(method = "render", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/client/render/TexturedRenderLayers;getSignTextureId(Lnet/minecraft/util/SignType;)Lnet/minecraft/client/util/SpriteIdentifier;"))
+    public SpriteIdentifier useCanvasSignSprite(SignType signType) {
         if (sign.getCachedState().getBlock() instanceof ICanvasSign canvasSign) {
             return CanvasSignBlockEntityRenderer.getCanvasSignSpriteTexture(canvasSign.getBackgroundColor());
         }
 
-        return signSprite;
+        return TexturedRenderLayers.getSignTextureId(signType);
     }
 
 }
