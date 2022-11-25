20d41eaef61e3b747bffb319dcd822f9ceec656a
blu3potato <112722946+blu3potato@users.noreply.github.com>
2022-11-25 22:51:48 +0800

    Fix Foods and misc
    - Remove old Raw Pasta and Wheat Dough recipes and recipe unlocks
    - Meat Cuts, Cabbage Leaf, and Kelp Roll Slice can be eaten quickly
    - Fried Rice grants Comfort instead of Nourishment
    - Glow Berry Custard can be eaten on full hunger
    - Budding Tomatoes 3rd Stage Shape is fixed from 1 pixel high to 10 pixel high
    - Birch Cabinet is now listed after Spruce Cabinet

--- src/main/java/com/nhoryzon/mc/farmersdelight/item/enumeration/Foods.java
+++ src/main/java/com/nhoryzon/mc/farmersdelight/item/enumeration/Foods.java
@@ -25,29 +25,29 @@ public enum Foods {
     RAW_PASTA(2, .3f, () -> new StatusEffectInstance(StatusEffects.HUNGER, ConsumableItem.BRIEF_DURATION, 0), .3f),
     PIE_CRUST(2, .2f),
     PUMPKIN_SLICE(3, .3f),
-    CABBAGE_LEAF(1, .4f),
-    MINCED_BEEF(2, .3f, true),
-    BEEF_PATTY(4, .8f, true),
-    CHICKEN_CUTS(1, .3f, () -> new StatusEffectInstance(StatusEffects.HUNGER, ConsumableItem.BRIEF_DURATION, 0), .3f, true),
-    COOKED_CHICKEN_CUTS(3, .6f, true),
-    BACON(2, .3f, true),
-    COOKED_BACON(4, .8f, true),
-    COD_SLICE(1, .1f),
-    COOKED_COD_SLICE(3, .5f),
-    SALMON_SLICE(1, .1f),
-    COOKED_SALMON_SLICE(3, .8f),
-    MUTTON_CHOP(1, .3f, true),
-    COOKED_MUTTON_CHOP(3, .8f, true),
+    CABBAGE_LEAF(1, .4f, false, true),
+    MINCED_BEEF(2, .3f, true, true),
+    BEEF_PATTY(4, .8f, true, true),
+    CHICKEN_CUTS(1, .3f, () -> new StatusEffectInstance(StatusEffects.HUNGER, ConsumableItem.BRIEF_DURATION, 0), .3f, true, true, false),
+    COOKED_CHICKEN_CUTS(3, .6f, true, true),
+    BACON(2, .3f, true, true),
+    COOKED_BACON(4, .8f, true, true),
+    COD_SLICE(1, .1f, false, true),
+    COOKED_COD_SLICE(3, .5f, false, true),
+    SALMON_SLICE(1, .1f, false, true),
+    COOKED_SALMON_SLICE(3, .8f, false, true),
+    MUTTON_CHOP(1, .3f, true, true),
+    COOKED_MUTTON_CHOP(3, .8f, true, true),
     HAM(5, .3f),
     SMOKED_HAM(10, .8f),
 
     // Sweets
     POPSICLE(3, .2f, null, .0f, false, true, true),
-    COOKIES(2, .1f, null, .0f, false, true, false),
+    COOKIES(2, .1f, false, true),
     CAKE_SLICE(2, .1f, () -> new StatusEffectInstance(StatusEffects.SPEED, 400, 0), 1.f, false, true, false),
     PIE_SLICE(3, .3f, () -> new StatusEffectInstance(StatusEffects.SPEED, ConsumableItem.BRIEF_DURATION, 0), 1.f, false, true, false),
     FRUIT_SALAD(6, .6f, () -> new StatusEffectInstance(StatusEffects.REGENERATION, 100, 0), 1.f),
-    GLOW_BERRY_CUSTARD(7, .6f, () -> new StatusEffectInstance(StatusEffects.GLOWING, 100, 0), 1.f),
+    GLOW_BERRY_CUSTARD(7, .6f, () -> new StatusEffectInstance(StatusEffects.GLOWING, 100, 0), 1.f, false, false, true),
 
     // Handheld Foods
     MIXED_SALAD(6, .6f, () -> new StatusEffectInstance(StatusEffects.REGENERATION, 100, 0), 1.f),
@@ -64,7 +64,7 @@ public enum Foods {
     SALMON_ROLL(7, .6f),
     COD_ROLL(7, .6f),
     KELP_ROLL(12, .6f),
-    KELP_ROLL_SLICE(6, .5f),
+    KELP_ROLL_SLICE(6, .5f, false, true),
 
     // Bowl Foods
     COOKED_RICE(6, .4f, () -> new StatusEffectInstance(EffectsRegistry.COMFORT.get(), ConsumableItem.BRIEF_DURATION, 0), 1.f),
@@ -73,13 +73,13 @@ public enum Foods {
     VEGETABLE_SOUP(12, .8f, () -> new StatusEffectInstance(EffectsRegistry.COMFORT.get(), ConsumableItem.MEDIUM_DURATION, 0), 1.f),
     FISH_STEW(12, .8f, () -> new StatusEffectInstance(EffectsRegistry.COMFORT.get(), ConsumableItem.MEDIUM_DURATION, 0), 1.f),
     CHICKEN_SOUP(14, .75f, () -> new StatusEffectInstance(EffectsRegistry.COMFORT.get(), ConsumableItem.LONG_DURATION, 0), 1.f),
-    FRIED_RICE(14, .75f, () -> new StatusEffectInstance(EffectsRegistry.NOURISHMENT.get(), ConsumableItem.LONG_DURATION, 0), 1.f),
+    FRIED_RICE(14, .75f, () -> new StatusEffectInstance(EffectsRegistry.COMFORT.get(), ConsumableItem.LONG_DURATION, 0), 1.f),
     PUMPKIN_SOUP(14, .75f, () -> new StatusEffectInstance(EffectsRegistry.COMFORT.get(), ConsumableItem.LONG_DURATION, 0), 1.f),
     BAKED_COD_STEW(14, .75f, () -> new StatusEffectInstance(EffectsRegistry.COMFORT.get(), ConsumableItem.LONG_DURATION, 0), 1.f),
     NOODLE_SOUP(14, .75f, () -> new StatusEffectInstance(EffectsRegistry.COMFORT.get(), ConsumableItem.LONG_DURATION, 0), 1.f),
 
     // Plated Foods
-    BACON_AND_EGGS(10, .6f, () -> new StatusEffectInstance(EffectsRegistry.NOURISHMENT.get(), ConsumableItem.SHORT_DURATION, 0), 12.f),
+    BACON_AND_EGGS(10, .6f, () -> new StatusEffectInstance(EffectsRegistry.NOURISHMENT.get(), ConsumableItem.SHORT_DURATION, 0), 1.f),
     RATATOUILLE(10, .6f, () -> new StatusEffectInstance(EffectsRegistry.NOURISHMENT.get(), ConsumableItem.SHORT_DURATION, 0), 1.f),
     STEAK_AND_POTATOES(12, .8f, () -> new StatusEffectInstance(EffectsRegistry.NOURISHMENT.get(), ConsumableItem.MEDIUM_DURATION, 0), 1.f),
     PASTA_WITH_MEATBALLS(12, .8f, () -> new StatusEffectInstance(EffectsRegistry.NOURISHMENT.get(), ConsumableItem.MEDIUM_DURATION, 0), 1.f),
@@ -96,7 +96,7 @@ public enum Foods {
     HONEY_GLAZED_HAM(14, .75f, () -> new StatusEffectInstance(EffectsRegistry.NOURISHMENT.get(), ConsumableItem.LONG_DURATION, 0), 1.f),
     SHEPHERDS_PIE(14, .75f, () -> new StatusEffectInstance(EffectsRegistry.NOURISHMENT.get(), ConsumableItem.LONG_DURATION, 0), 1.f),
 
-    DOG_FOOD(4, .2f, true);
+    DOG_FOOD(4, .2f, true, false);
 
     private final Supplier<FoodComponent> food;
 
@@ -104,16 +104,12 @@ public enum Foods {
         this(hunger, saturation, null, .0f, false, false, false);
     }
 
-    Foods(int hunger, float saturation, boolean isMeat) {
-        this(hunger, saturation, null, .0f, isMeat, false, false);
+    Foods(int hunger, float saturation, boolean isMeat, boolean isFastToEat) {
+        this(hunger, saturation, null, .0f, isMeat, isFastToEat, false);
     }
 
     Foods(int hunger, float saturation, Supplier<StatusEffectInstance> effect, float effectChance) {
         this(hunger, saturation, effect, effectChance, false, false, false);
-    }
-
-    Foods(int hunger, float saturation, Supplier<StatusEffectInstance> effect, float effectChance, boolean isMeat) {
-        this(hunger, saturation, effect, effectChance, isMeat, false, false);
     }
 
     Foods(int hunger, float saturation, Supplier<StatusEffectInstance> effect, float effectChance, boolean isMeat, boolean isFastToEat, boolean alwaysEdible) {
