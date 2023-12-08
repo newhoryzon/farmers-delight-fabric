8b817a7285db5f7fb3b902b24937a61a3b0a9e2a
KEVIN BEAUCORAL <beaucoral.kevin@gmail.com>
2023-12-08 13:48:58 +0100

    Merge pull request #220 from BarchamMal/master
    Fix some several easy-to-fix issues.

--- src/main/java/com/nhoryzon/mc/farmersdelight/mixin/AnyPlantOnRichSoilFarmLandMixin.java
+++ src/main/java/com/nhoryzon/mc/farmersdelight/mixin/AnyPlantOnRichSoilFarmLandMixin.java
@@ -1,8 +1,7 @@
 package com.nhoryzon.mc.farmersdelight.mixin;
 
 import com.nhoryzon.mc.farmersdelight.registry.BlocksRegistry;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.CropBlock;
+import net.minecraft.block.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,10 +9,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(CropBlock.class)
-public class CropBlockOnRichSoilFarmlandMixin {
+@Mixin({CropBlock.class, AttachedStemBlock.class, StemBlock.class})
+public class AnyPlantOnRichSoilFarmLandMixin {
+
     @Inject(at = @At("TAIL"), method = "canPlantOnTop", cancellable = true)
-    private void cropBlockCanPlantOnTopOfRichSoil(BlockState floor, BlockView world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
-        cir.setReturnValue(cir.getReturnValue() || floor.isOf(BlocksRegistry.RICH_SOIL_FARMLAND.get()));
+    private void pitcherCropBlockCanPlantOnTopOfRichSoil(BlockState floor, BlockView world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+        if (floor.isOf(BlocksRegistry.RICH_SOIL_FARMLAND.get())) {
+            cir.setReturnValue(true);
+        }
     }
 }
